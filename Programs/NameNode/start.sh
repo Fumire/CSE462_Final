634802975d9fff88333859949e70e8f5da76fd99
Jaewoong Lee <230@fumire.moe>
2020-05-25 21:35:25 +0900

    Try to launch NameNode

--- Programs/NameNode/start.sh
+++ Programs/NameNode/start.sh
@@ -6,7 +6,7 @@ if [ "$(ls -A $NAME_DIR)" ]; then
     echo "NameNode is already formatted."
 else
     echo "Format NameNode."
-    $HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
+    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
 fi
 
-$HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR namenode
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
