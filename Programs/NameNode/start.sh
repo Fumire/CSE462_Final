7901200123593b5147b31b7131442ab7cc80bb06
Jaewoong Lee <230@fumire.moe>
2020-05-29 23:25:03 +0900

    Port is changed from version 3

--- Programs/NameNode/start.sh
+++ Programs/NameNode/start.sh
@@ -2,6 +2,9 @@
 NAME_DIR=$1
 echo $NAME_DIR
 
+echo "export JAVA_HOME=${JAVA_HOME}" >> /etc/profile
+source /etc/profile
+
 if [ "$(ls -A $NAME_DIR)" ]; then
     echo "NameNode is already formatted."
 else
@@ -9,4 +12,9 @@ else
     $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
 fi
 
+#$HADOOP_HOME/bin/hdfs --daemon start --config $HADOOP_CONF_DIR journalnode
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
+#$HADOOP_HOME/bin/hdfs --daemon start --config $HADOOP_CONF_DIR zkfc
+#$HADOOP_HOME/sbin/hadoop-daemons.sh start datanode
+#$HADOOP_HOME/sbin/start-yarn.sh
+#$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
