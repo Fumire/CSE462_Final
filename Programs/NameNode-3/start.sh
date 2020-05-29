a8da09fc628c974acdd24f6ab8ebca3b8fd747d6
Jaewoong Lee <230@fumire.moe>
2020-05-30 04:24:15 +0900

    Remove and add

--- Programs/NameNode-3/start.sh
+++ Programs/NameNode-3/start.sh
@@ -12,9 +12,5 @@ else
     $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
 fi
 
-#$HADOOP_HOME/bin/hdfs --daemon start --config $HADOOP_CONF_DIR journalnode
+$HADOOP_HOME/bin/mapred --daemon start historyserver
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
-#$HADOOP_HOME/bin/hdfs --daemon start --config $HADOOP_CONF_DIR zkfc
-#$HADOOP_HOME/sbin/hadoop-daemons.sh start datanode
-#$HADOOP_HOME/sbin/start-yarn.sh
-#$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
