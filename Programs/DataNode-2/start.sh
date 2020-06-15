3a085bb89dc99a7154631d3805d5538cb0b93308
Jaewoong Lee <230@fumire.moe>
2020-06-16 05:22:28 +0900

    Add version 2

---
+++ Programs/DataNode-2/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "export JAVA_HOME=${JAVA_HOME}" >> /etc/profile
+. /etc/profile
+
+service sysstat start
+cron
+
+$HADOOP_HOME/bin/yarn --daemon start --config $HADOOP_CONF_DIR nodemanager
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
---
+++ Programs/NameNode-2/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+NAME_DIR=$1
+echo $NAME_DIR
+
+echo "export JAVA_HOME=${JAVA_HOME}" >> /etc/profile
+source /etc/profile
+
+service ssh start
+service sysstat start
+cron
+
+if [ "$(ls -A $NAME_DIR)" ]; then
+    echo "NameNode is already formatted."
+else
+    echo "Format NameNode."
+    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
+fi
+
+$HADOOP_HOME/bin/yarn --daemon start --config $HADOOP_CONF_DIR resourcemanager
+$HADOOP_HOME/bin/mapred --daemon start historyserver
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
