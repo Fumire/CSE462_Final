49d8ac70b970d717d089945369bcc1fe235d820d
Jaewoong Lee <230@fumire.moe>
2020-06-19 02:43:12 +0900

    Edit for version 2

--- Programs/DataNode-2/start.sh
+++ Programs/DataNode-2/start.sh
@@ -5,5 +5,5 @@ echo "export JAVA_HOME=${JAVA_HOME}" >> /etc/profile
 service sysstat start
 cron
 
-$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR nodemanager
+$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR nodemanager &
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
